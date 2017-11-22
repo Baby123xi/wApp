7a095d8a304af48afa4c59bdf9c8d31b183ac742
RenrenWang <1011420784@qq.com>
2017-11-22 17:11:48 +0800

    dasdadsd

--- js/Dao/index.js
+++ js/Dao/index.js
@@ -1,8 +1,17 @@
-const ToDoList={
-    
-}
+const ToDoList = [
+    { title: "标题1", time: '2017-08-06', status: 0 },
+    { title: "标题2", time: '2017-08-06', status: 1 },
+    { title: "标题3", time: '2017-08-06', status: 0 },
+    { title: "标题4", time: '2017-08-06', status: 0 },
+    { title: "标题5", time: '2017-08-06', status: 0 },
+    { title: "标题6", time: '2017-08-06', status: 1 },
+    { title: "标题7", time: '2017-08-06', status: 0 },
+    { title: "标题8", time: '2017-08-06', status: 0 },
+    { title: "标题9", time: '2017-08-06', status: 1 },
+    { title: "标题10", time: '2017-08-06', status: 1 },
+];
 
 
-export default{
-  
+export default {
+   ToDoList
 }
--- js/containers/ToDoList/index.js
+++ js/containers/ToDoList/index.js
@@ -1,34 +1,38 @@
 import React, { Component } from 'react'
 import {
-
+Dimensions,
   View,
   Image,
   Text,
 FlatList
 } from 'react-native'
 
-
+const { width, height } = Dimensions.get('window')
 import Header from '../../components/Header'
+import Dao from '../../Dao'
 export default class extends Component {
 
- renderItem(item){
+ renderItem(data){
+  let item=data.item;
   return(
-      <View style={{height:100,flexDirection:'row',justifyContent:'space-between',alignItems:'center'}}>
+      <View  style={{borderBottomWidth:1,borderColor:'#eee',backgroundColor:'#fff',paddingHorizontal:15,width,height:85,flexDirection:'row',justifyContent:'space-between',alignItems:'center'}}>
             <View style={{flex:1,flexDirection:'column'}}>
                  <Text style={{fontSize:16,fontWeight:'bold',color:'#222'}}>{item.title}</Text>
-                 <Text style={{fontSize:14,color:'#bbb',marginTop:10}}>{item.time}</Text>
+                 <Text style={{fontSize:14,color:'#bbb',marginTop:15}}>{item.time}</Text>
             </View>
             <Text style={{fontSize:16,color:item.status==0?'#f00':'#4c8def'}}>{item.status==0?'未完成':'已完成'}</Text>
       </View>
   )
  }
   render(){
+  
     return(
       <View style={{flex:1}}>
             <Header title={"待办事件"} isSub={true} leftBtnAction={()=>this.props.navigation.goBack()}/>
             <FlatList
-                data={[{key: 'a'}, {key: 'b'}]}
-                renderItem={({item}) => <Text>{item.key}</Text>}
+                data={Dao.ToDoList}
+                renderItem={(item)=>this.renderItem(item)}
+                keyExtractor={(item, index) => index}
              />
       </View>
     )
